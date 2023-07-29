4fcaed34d10b7e8eb8e887f72530d5d5eb744929
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-07-29 00:40:24 -0400

    first pass on repeat message

--- client/components/AlarmCard.jsx
+++ client/components/AlarmCard.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import useEffect from 'react'
 
-function AlarmCard({ enabled, time, rptDays, label }) { 
+function AlarmCard({ enabled, time, rptDays, label }) {
+
+    let rptMessage
+
+    if (rptDays.length == 0) {
+        rptMessage = "One-time"
+    }
+    else {
+        rptMessage = "otherwise"
+    }
 
   return (
     <div>
         { enabled ? "True" : "False" }
+        <br/>
         { time }
+        <br/>
+        { rptMessage }
+        <br/>
         { label }
+        <br/>
     </div>
   )
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
       <Next/>
       <Weather/>
       <AlarmContainer/>
-      <AlarmCard/>
     </>
   )
 }
