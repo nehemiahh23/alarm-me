e1918a05255bfbfc39a31f1778a925e281810259
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-08-01 18:05:06 -0400

    fixed render issue

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-function Weather() {
+function Weather({ lat, long, weather, setWeather }) {
+
+  const current = new Date()
+  const parsed = current.getHours()
+
+  useEffect(() => {
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York&forecast_days=1`)
+    .then(r => r.json())
+    .then(data => setWeather(data))
+  }, [])
+
+  console.log(weather)
+
   return (
     <>
-        Weather
+        <span> { weather.hourly.temperature_2m[parsed] } °F</span>
     </>
   )
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import { useState } from "react"
 import Next from "../components/Next.jsx"
 import Weather from "../components/Weather.jsx"
 import AlarmContainer from "../components/AlarmContainer.jsx"
@@ -6,10 +6,34 @@ import AlarmCard from "../components/AlarmCard.jsx"
 
 function App() {
 
+  const [lat, setLat] = useState(0)
+  const [long, setLong] = useState(0)
+  const [weather, setWeather] = useState({
+    hourly: {
+      temperature_2m: []
+    }
+  })
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    console.log("Geolocation not supported");
+  }
+  
+  function success(position) {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+    console.log(`Latitude: ${lat}, Longitude: ${long}`);
+  }
+  
+  function error() {
+    console.log("Unable to retrieve your location");
+  }
+
   return (
     <>
       <Next/>
-      <Weather/>
+      { lat && long ? <Weather lat={lat} long={long} weather={weather} setWeather={setWeather}/> : "Loading..."}
       <AlarmContainer/>
     </>
   )
