0666bae80c6ce1f3ed3dd38baa6c7b00589f4582
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-08-02 14:40:22 -0400

    convert db time to timestamp format

--- client/components/AlarmCard.jsx
+++ client/components/AlarmCard.jsx
@@ -2,52 +2,55 @@ import { useState } from 'react'
 
 function AlarmCard({ id, enabled, time, rptDays, label }) {
 
-    // #region rptDays
-    let rptMessage = ""
-    const daysArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  // #region rptDays
+  let rptMessage = ""
+  const daysArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-    if (rptDays.length == 0) {
-        rptMessage = "One-time"
-    }
-    else if (rptDays.length == 5 && !rptDays.includes(1) && !rptDays.includes(7)) {
-      rptMessage = "Weekdays"
-    }
-    else if (rptDays.length == 2 && rptDays.includes(1) && rptDays.includes(7)) {
-      rptMessage = "Weekends"
-    }
-    else if (rptDays.length == 7) {
-      rptMessage = "Every day"
-    }
-    else {
-        rptDays.forEach(day => {
-          rptMessage += daysArr[day - 1]
-        })
-    }
-    // #endregion
+  if (rptDays.length == 0) {
+      rptMessage = "One-time"
+  }
+  else if (rptDays.length == 5 && !rptDays.includes(1) && !rptDays.includes(7)) {
+    rptMessage = "Weekdays"
+  }
+  else if (rptDays.length == 2 && rptDays.includes(1) && rptDays.includes(7)) {
+    rptMessage = "Weekends"
+  }
+  else if (rptDays.length == 7) {
+    rptMessage = "Every day"
+  }
+  else {
+      rptDays.forEach(day => {
+        rptMessage += daysArr[day - 1]
+      })
+  }
+  // #endregion
 
-    // #region enabled
-    const [toggle, setToggle] = useState(enabled)
+  // #region enabled
+  const [toggle, setToggle] = useState(enabled)
 
-    function clickHandler() {
-      setToggle(!toggle)
+  function clickHandler() {
+    setToggle(!toggle)
 
-      // fetch(`http://localhost:3002/alarms/${id}`, {
-      //     method: 'PATCH',
-      //     headers: {
-      //         'Content-Type': 'application/json',
-      //         'Accepts': 'application/json'
-      //     },
-      //     body: JSON.stringify({enabled: toggle})
-      // })
-      // .then(r => r.json)
-    }
-    // #endregion
+    // fetch(`http://localhost:3002/alarms/${id}`, {
+    //     method: 'PATCH',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Accepts': 'application/json'
+    //     },
+    //     body: JSON.stringify({enabled: toggle})
+    // })
+    // .then(r => r.json)
+  }
+  // #endregion
+
+  const date = new Date(time)
+  const hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
   return (
     <div>
         <span onClick={clickHandler}>{ toggle ? "✅" : "❌" }</span>
         <br/>
-        <h2>{ time }</h2>
+        <h2>{ `${hours[date.getHours()]}:${date.getMinutes() == 0 ? "00" : date.getMinutes()} ${date.getHours() >= 12 ? "PM" : "AM"}` }</h2>
         <br/>
         { rptMessage }
         <br/>
